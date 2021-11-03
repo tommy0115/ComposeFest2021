c94f2823ef29d0e29be6e886a2e35ca56ca61a42
tmkwon <tmkwon@rsupport.com>
2021-11-03 12:36:29 +0900

    실습

---
+++ week 1-Jetpack Compose basics/BasicsCodelabStudy/app/src/main/java/com/codelab/basics/study/SystemUiHelper.kt
@@ -0,0 +1,25 @@
+package com.codelab.basics.study
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun SystemUi() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+
+    SideEffect {
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons
+        )
+
+        //systemUiController.isSystemBarsVisible = false
+        // setStatusBarsColor() and setNavigationBarsColor() also exist
+    }
+}
--- week 1-Jetpack Compose basics/BasicsCodelabStudy/app/src/main/java/com/codelab/basics/study/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/BasicsCodelabStudy/app/src/main/java/com/codelab/basics/study/ui/theme/Theme.kt
@@ -5,19 +5,23 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+/*private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = Color.Black
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = Color.LightGray
+)*/
 
-    /* Other default colors to override
+/* Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
@@ -25,12 +29,32 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    primaryVariant = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    primaryVariant = Navy,
+    onPrimary = Navy
 )
 
 @Composable
 fun BasicsCodelabStudyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
