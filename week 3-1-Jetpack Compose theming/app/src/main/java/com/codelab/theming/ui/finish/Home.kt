67e828d2a0f039225a1c2e01fbf242d2bd9bdf56
tmkwon <tree3480@gmail.com>
2021-12-02 22:07:52 +0900

    week3-1 study

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/finish/Home.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/finish/Home.kt
@@ -46,11 +46,13 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
